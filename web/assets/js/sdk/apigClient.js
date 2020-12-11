6a64bc7f65c14070ecc548995f2a00215ed54637
VibhaChoudhary <vc1436@nyu.edu>
2020-12-11 17:42:11 -0500

    Update api gateway url

--- web/assets/js/sdk/apigClient.js
+++ web/assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://vlzdlz1emi.execute-api.us-east-1.amazonaws.com/test';
+    var invokeUrl = 'https://5xg66x41b7.execute-api.us-east-1.amazonaws.com/test';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
