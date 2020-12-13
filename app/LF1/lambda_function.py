fe4b1a169ea5e5d7ec819ad0bd0f5484d057cfe6
VibhaChoudhary <vc1436@nyu.edu>
2020-12-13 18:49:06 -0500

    template files upload

--- app/LF1/lambda_function.py
+++ app/LF1/lambda_function.py
@@ -1,5 +1,131 @@
+import json
+import boto3
+import logging
+import base64
+from datetime import datetime
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import os.path
+import requests
+import re
+import os
+
+logger = logging.getLogger('index-photos')
+logger.setLevel(logging.DEBUG)
+
+def extend_slot_type(labels):
+    """Update the Lex search labels slot type with new labels for training purpose.
+     Args:
+         labels (list): detected labels list.
+    """
+    client = boto3.client('lex-models')
+    response = client.get_slot_type(
+        version='$LATEST',
+        name='SearchLabels',
+    )
+    logger.debug('response slot type %s', response)
+    values = []
+    new_labels = set()
+    for value in response['enumerationValues']:
+        new_labels.add(value['value'])
+    for label in labels:
+        new_labels.add(label)
+    for label in new_labels:    
+        value = {'value' : label}
+        values.append(value)
+    response = client.put_slot_type(
+        name='SearchLabels',
+        description='Search terms for photo album',
+        enumerationValues=values,
+        checksum=response['checksum'],
+    )
+    logger.debug('response slot type %s', response)
+
+def get_labels(image_json):
+    """Generates a list of labels using Amazon Rekognition for the input image.
+     Args:
+         image_json (object): image object.
+     Returns:
+         list: labels detected in the image
+     """
+    client = boto3.client('rekognition')
+    labels = []
+    try:
+        response = client.detect_labels(
+            Image=image_json,
+            MaxLabels=10,
+            MinConfidence=85
+        )
+        logger.debug('Response from detect labels: %s', json.dumps(response))
+    except Exception as e:
+        logger.error('Exception occurred while detecting labels %s', str(e))
+    else:
+        for label in response['Labels']:
+            labels.append(label['Name'])
+    return labels
+
+
+def upload_to_es(json_doc):
+    """Uploads image metadata to elastic search
+    Args:
+       json_obj (json): image metadata.
+    """
+    # ak = os.environ['AWS_ACCESS_KEY_ID']
+    # sk = os.environ['AWS_SECRET_ACCESS_KEY']
+    # st = os.environ['AWS_SESSION_TOKEN']
+    # region = 'us-east-1'
+    # service = 'es'
+    # awsauth = AWS4Auth(ak, sk, region, service, session_token=st)
+
+    host = u'search-photos-s4cwptwn6oydgvpt63ms6s6k6m.us-east-1.es.amazonaws.com'
+    doc = json.loads(json_doc)
+    key = doc['objectKey']
+    es = Elasticsearch(
+        hosts=[{u'host': host, u'port': 443}],
+        # http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection,
+        request_timeout=30
+    )
+    logger.debug(json_doc)
+    res = es.index(index='photo_metadata', doc_type='image', id = key, body=doc)
+    logger.debug(res)
+
 def lambda_handler(event, context):
+    records = event['Records']
+    for record in records:
+        # Get s3 details from event record
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        s3 = boto3.resource('s3')
+        object_acl = s3.ObjectAcl(bucket,key)
+        response = object_acl.put(ACL='public-read')
+        logger.debug(json.dumps(response))
+        
+        # Get labels from rekognition
+        image_json = {
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key
+            }
+        }
+        labels = get_labels(image_json)
+        
+        # Store information in elastic search
+        json_doc = {
+            "objectKey": key,
+            "bucket": bucket,
+            "createdTimestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+            "labels": labels
+        }
+        json_doc = json.dumps(json_doc)
+        upload_to_es(json_doc)
+        
+        # Extend slot type values
+        extend_slot_type(labels)
+        
     return {
         'statusCode': 200,
-        'body': json.dumps('Success')
+        'body': json.dumps('Success from LF1')
     }
--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,3 +1,95 @@
 $(document).ready(function() {
-  
+    $('.info').hide()
+    // Initialize the Amazon Cognito credentials provider
+    $('#stop-button').hide();
+    AWS.config.update({
+        credentials: new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'us-east-1:77379cb4-0a21-40b7-9eb8-bd30b979f294'
+        }),
+        region: 'us-east-1'
+    });
+    $("#file-upload").change(function(){
+        //submit the form here
+        var files = document.getElementById('file-upload').files;
+        console.log(files)
+
+        for(let i=0, file; file=files[i]; i++){
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                console.log("reading of" + file.name)
+                let fileName = file.name;
+                let imagebase64 = e.target.result.replace(/^data:image\/(png|jpg|jpeg);base64,/,"");
+                uploadImage(imagebase64, fileName).
+                then((response) => {
+                    console.log("Response", response);
+
+                })
+                .catch((error) => {
+                    console.log('An error occurred while uploading image', error);
+                });
+            };
+            console.log("handling of " + file.name)
+            reader.readAsDataURL(file);
+        }
+        $(".info").text(files.length + " file(s) uploaded successfully");
+        $('#file-upload').val('')
+        $('.info').show()
+
+    });
+
+    $('.upload-button').click(function(){
+        $('#file-upload').trigger('click');
+    });
+
+    $('.main').click(function(){
+        $('.info').hide()
+    });
+
+    $('.search-button').click(function(){
+        const input = $('#search-input').val();
+        if ($.trim(input) === '') {
+            return false;
+        }
+        $('#search-input').val('');
+        console.log(input)
+        searchImages(input).
+        then((response) => {
+            $('.images').html('');
+            console.log("Response", response);
+            res = response["data"]["results"];
+            if(res === undefined){
+                console.log("Result undefined");
+            } else {
+                console.log(res.length + "images found");
+                $('.images').append('<p>' + res.length + ' search results for <b>' + input + '</b></p>');
+                for(i=0;i<res.length;i++){
+                    console.log(res[i]['url']);
+                    $(".images").append("" +
+                        "<img src='" +
+                        res[i]['url'] +
+                        "' class = 'p-2' style='height: 200px;width: 220px'/>");
+                }
+            }
+        })
+        .catch((error) => {
+            console.log('An error occurred while searching images', error);
+        });
+
+
+    });
+    function uploadImage(image, filename) {
+        return sdk.uploadFilenamePut(
+            {'filename': filename},
+            image,
+            {});
+    }
+
+    function searchImages(query) {
+        return sdk.searchGet(
+            {},
+             {},
+            {queryParams: {
+                    'q': query
+                }});
+    }
 });
